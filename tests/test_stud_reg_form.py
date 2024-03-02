5e570482a474a1bcdff480d00bdff09e834d3b3d
Max Rusin <maxunris@gmail.com>
2024-03-02 21:17:41 +0300

    final

--- tests/test_stud_reg_form.py
+++ tests/test_stud_reg_form.py
@@ -1,33 +1,10 @@
 from demoqa_tests.pages.registration_page import RegistrationPage
+from demoqa_tests.data import users
 
 
 def test_user_registration_form():
+    client = users.user
     registration_pages = RegistrationPage()
     registration_pages.open()
-    registration_pages.fill_first_name('Max')
-    registration_pages.fill_last_name('Cheshire')
-    registration_pages.fill_email('Maxcheshire1@gmail.com')
-    registration_pages.fill_gender('Male')
-    registration_pages.fill_phone('7999123121')
-    registration_pages.fill_date_of_birth("26", "March", "1998")
-    registration_pages.fill_subjects('Computer Science')
-    registration_pages.fill_hobbies('Reading')
-    registration_pages.attach_photo('123.jpeg')
-    registration_pages.fill_current_address('Pushkina-kolotushkina, Moscow, Russia')
-    registration_pages.fill_state('NCR')
-    registration_pages.fill_city('Delhi')
-    registration_pages.sumbit()
-    registration_pages.should_registration_form(
-        'Max',
-        'Cheshire',
-        'Maxcheshire1@gmail.com',
-        'Male',
-        '7999123121',
-        '26 March,1998',
-        'Computer Science',
-        'Reading',
-        '123.jpeg',
-        'Pushkina-kolotushkina, Moscow, Russia',
-        'NCR',
-        'Delhi',
-    )
+    registration_pages.registration_form_page(client)
+    registration_pages.should_registration_form(client)
